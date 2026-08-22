a3ded169ab453d0c85afb368d4958c07979e467e
anth0ny-527 <a.zampino3@gmail.com>
2026-08-22 16:11:17 -0400

    Basic menu added

---
+++ team3-proj1/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject helpScreen;
+    public GameObject creditsScreen;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        helpScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+
+    public void OpenHelp()
+    {
+        mainMenu.SetActive(false);
+        helpScreen.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        creditsScreen.SetActive(true);
+    }
+
+    public void ReturnToMainMenu()
+    {
+        helpScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
