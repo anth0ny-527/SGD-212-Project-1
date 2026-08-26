5f657b94b088d196ea35c607838c372816bc7f9e
JacksonHaycraftGithub <jacksonmhaycraft@gmail.com>
2026-08-26 19:13:22 -0400

    Slight Tweak

--- team3-proj1/Assets/Scripts/Firewall.cs
+++ team3-proj1/Assets/Scripts/Firewall.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class Firewall : MonoBehaviour
 {
+    [SerializeField] private float fireWallSpeed = 0.02f;
     IEnumerator MoveFireWall()
     {
         do
         {
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + 0.02f);
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z + fireWallSpeed);
             yield return new WaitForSeconds(0.01f);
         } while (true);
     }
