74e3e69569a26fb8aa5d58c282f36a6825da0231
eduavell <edu9268@gmail.com>
2026-08-27 18:35:13 -0400

    Created TimeManager

---
+++ team3-proj1/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeManager : MonoBehaviour
+{
+    private float levelOneTime, levelTwoTime, levelThreeTime = 0.0f;
+    private float elapsedTime = 0.0f;
+    private bool isTimerRunning = false;
+    public static TimeManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (isTimerRunning)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+    }
+
+    public void SetUpBestTime(string sceneName)
+    {
+        if (sceneName == "LevelOne")
+        {
+            levelOneTime = elapsedTime;
+        }
+        else if (sceneName == "LevelTwo")
+        {
+            levelTwoTime = elapsedTime;
+        }
+        else if (sceneName == "LevelThree")
+        {
+            levelThreeTime = elapsedTime;
+        }
+    }
+
+    public void StartTimer() { isTimerRunning = true; }
+
+    public void PauseTimer() { isTimerRunning = false; }
+
+    public void ResetTimer() { elapsedTime = 0.0f; }
+}
